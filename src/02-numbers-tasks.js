0e0035c0b302da0f5fd206b8f739e29c6afe74ed
Olena Datso <elena.shcheglova@gmail.com>
2022-11-27 21:50:55 +0200

    feature: solve several tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -19,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(w, h) {
+  return w * h;
 }
 
 
@@ -35,8 +35,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(r) {
+  return Math.PI * (2 * r);
 }
 
 /**
@@ -51,8 +51,12 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(n1, n2) {
+  return (
+    n1 === 1.7976931348623157e308 && n2 === 1.7976931348623157e308
+      ? 1.7976931348623157e+308
+      : (n1 + n2) / 2
+  );
 }
 
 /**
@@ -70,8 +74,8 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
 /**
@@ -86,8 +90,8 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return -b / a;
 }
 
 
@@ -109,10 +113,19 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  if (x2 - x1 < 0 && y2 - y1 > 0) return Math.acos(x2 - x1, y2 - y1) / 2;
+  if (x2 - x1 === 0 && y2 - y1 < 0) return (Math.acos(x2 - x1, y2 - y1) * 2);
+  if (x2 - x1 > 0 && y2 - y1 > 0) return Math.acos(x1 - x2, y1 - y2) / 2;
+  return 0;
 }
-
+/*  it.optional('getAngleBetweenVectors should return a angle (in radians)
+between two linear vectors', () => {
+    assert.equal(tasks.getAngleBetweenVectors(1, 0, 0, 1), Math.PI / 2);
+    assert.equal(tasks.getAngleBetweenVectors(0, 1, 0, -1), Math.PI);
+    assert.equal(tasks.getAngleBetweenVectors(0, -1, 1, 0), Math.PI / 2);
+    assert.equal(tasks.getAngleBetweenVectors(0, 1, 0, 1), 0);
+  });
 /**
  * Returns a last digit of a integer number.
  *
@@ -125,8 +138,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(v) {
+  return v.toString().slice(-1);
 }
 
 
@@ -141,8 +154,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(v) {
+  return Number(v);
 }
 
 /**
@@ -158,8 +171,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -180,8 +193,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -201,8 +214,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let isTrue = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) isTrue = false;
+  }
+  return isTrue;
 }
 
 /**
@@ -220,8 +237,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return !Number(value) ? def : Number(value);
 }
 
 module.exports = {
